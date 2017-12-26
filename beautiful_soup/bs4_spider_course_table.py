a2a8be7b881b1938c441dfeebf06849e0d515d5c
TauWu <1152876197@qq.com>
2017-12-26 18:58:04 +0800

    获取课表信息

--- beautiful_soup/bs4_spider_course_table.py
+++ beautiful_soup/bs4_spider_course_table.py
@@ -7,6 +7,8 @@ import requests
 from bs4 import BeautifulSoup
 from constant.request_const import request_methods, raw_url
 
+import re
+
 def get_course(card_no, academic_year):
     """获取课表(GET)
     """
@@ -19,7 +21,6 @@ def post_course(card_no, academic_year):
     """
     post_data = {"queryStudentId":card_no,"queryAcademicYear":academic_year}
     return requests.post(raw_url, data=post_data).text
-
 
 def request_course(card_no, academic_year, method):
     """发起课表请求
@@ -42,11 +43,29 @@ def get_beautiful_soup(card_no, academic_year, method):
     """
     return BeautifulSoup(request_course(card_no, academic_year, method))
 
-if __name__ == "__main__":
-    bsObj = get_beautiful_soup("213122983","17-18-2",1)
+def get_course_struct(bsList):
+    """传入经过网页分析的bsList,进一步分析变成可用的结构体
+    """
+    m = re.findall("<td.+rowspan=\".\">(.+)</td>",str(bsList))
+    s = m[0].split("<br/>")
+    if s.count("\xa0") == 0:
+        return
+    return s
+
+def get_course_list(bsObj):
+    """通过BeautifulSoup对象解析出课程列表
+    """
+    course_list = list()
     # 获取上午下午课程
     for i in bsObj.findAll("td",{"class":"line_topleft","rowspan":"5","align":"center"}):
-        print(i,'\n')
+        s = get_course_struct(i)
+        if s: course_list.append(s[:-1])
     # 获取晚上周末课程
     for i in bsObj.findAll("td",{"class":"line_topleft","rowspan":"2","align":"center"}):
-        print(i,'\n')
+        s = get_course_struct(i)
+        if s: course_list.append(s[:-1])
+    return course_list
+
+if __name__ == "__main__":
+    bsObj = get_beautiful_soup("213162983","17-18-2",1)
+    print(get_course_list(bsObj))
