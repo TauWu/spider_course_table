41b0c444425f2d66e045d6698ceb6e3220d4676a
TauWu <1152876197@qq.com>
2017-12-27 16:24:21 +0800

    课表信息结构化

--- beautiful_soup/bs4_spider_course_table.py
+++ beautiful_soup/bs4_spider_course_table.py
@@ -9,6 +9,8 @@ from constant.request_const import request_methods, raw_url
 
 import re
 
+from pprint import pprint
+
 def get_course(card_no, academic_year):
     """获取课表(GET)
     """
@@ -66,6 +68,49 @@ def get_course_list(bsObj):
         if s: course_list.append(s[:-1])
     return course_list
 
+def get_course_dict(course_list):
+    """通过课程列表解析出课程字典
+    最终生成的结构体结构如下:
+    {
+        "1":[
+            {
+                "name":"xxx",
+                "start_week":1,
+                "end_week":16,
+                "start_time":1,
+                "start_time":;3
+                "location":"J1-203"
+            }
+        ]
+    }
+    """
+    count = 1
+    # 存放所有课程信息数据
+    course_dict = dict()
+    # 存放时间区段课程数据
+    course_part_list = list()
+    # 存放单节课程数据
+    course_single = dict()
+
+    for course in course_list:
+        course_part_list = []
+        if len(course) == 0:
+            course_part_list = []
+        else:
+            for i in range(0,len(course),3):
+                course_single = {}
+                course_single["name"] = course[i]
+                result = re.findall("\[(.+)\-(.+)周\](.+)\-(.+)节", course[i+1])
+                course_single["start_week"] = result[0][0]
+                course_single["end_week"] = result[0][1]
+                course_single["start_time"] = result[0][2]
+                course_single["end_time"] = result[0][3]
+                course_single["location"] = course[i+2]
+                course_part_list.append(course_single)
+        course_dict[str(count)] = course_part_list
+        count = count + 1
+    return course_dict
+
 if __name__ == "__main__":
     bsObj = get_beautiful_soup("213162983","17-18-2",1)
-    print(get_course_list(bsObj))
+    print(get_course_dict(get_course_list(bsObj)))
