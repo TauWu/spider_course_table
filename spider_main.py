d1722b9111779f7900253d169b5b68c77e38876c
TauWu <1152876197@qq.com>
2018-01-03 16:26:20 +0800

    添加Re模块识别课表方法

--- spider_main.py
+++ spider_main.py
@@ -3,6 +3,7 @@
 
 import sys
 from beautiful_soup.bs4_spider_course_table import bs4_main
+from spider_re.re_spider_course_table import re_main
 from util.logger import log_base
 
 if __name__ == "__main__":
@@ -16,5 +17,7 @@ if __name__ == "__main__":
         academic_year = sys.argv[4].replace("_","-")
         if method == 1:
             bs4_main(academic_year, start_cardno, end_cardno, logger)
+        elif method == 2:
+            re_main(academic_year, start_cardno, end_cardno, logger)
         else:
             logger.err("不存在这种操作!")
--- spider_re/re_spider_course_table.py
+++ spider_re/re_spider_course_table.py
@@ -9,19 +9,45 @@ import re, json
 from util.request_jwc import request_course
 from util.data_struct import get_course_dict
 from util.db_controller import insert_func
-from util.logger import insert_log
+from pprint import pprint
 
 # 错误模块
 from requests.exceptions import ConnectionError
+
+
+def get_course_struct(re_list):
+    """解析获得的区块课程信息
+    """
+    s = re_list.split("<br>")
+    if s.count("&nbsp;") == 0:
+        return
+    return s
 
 # 使用Re解析获取到的网页数据
 def get_course_list(card_no, academic_year, method):
     course_list = list()
     resp = request_course(card_no, academic_year, method)
-    print(resp)
-    # 获取上午下午课程
-    # re.findall()
-    # 获取周末晚上课程
+    course_day = re.findall(r"""<td rowspan="5" class="line_topleft" align="center">(.+)</td>""",resp)
+    course_night = re.findall(r"""<td class="line_topleft" rowspan="2"   align="center">(.+)</td>""", resp)
+    for i in course_day:
+        s = get_course_struct(i)
+        if s: course_list.append(s[:-1])
+    for i in course_night:
+        s = get_course_struct(i)
+        if s: course_list.append(s[:-1])
+    course_dict = get_course_dict(course_list)
+    return course_dict
 
-if __name__ == "__main__":
-    get_course_list(213151001, "17-18-1", 1)
+def re_main(academic_year, start_cardno, end_cardno, logger):
+    """使用Re模块匹配课表主程序
+    """
+    try:
+        for i in range(start_cardno, end_cardno):
+            course_list = get_course_list(i, academic_year, 1)
+            insert_func(i, academic_year, course_list, logger)
+    except IndexError:
+        logger.err("数据解析错误\t%d"%i)
+    except ConnectionError:
+        logger.err("网络连接错误\t%d"%i)
+    else:
+        pass
